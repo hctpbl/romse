4a7680eae64eb22ae1253feb0416eaf3876c83fd
Natalia <Natalia@Natalia>
2014-01-24 23:25:06 +0100

    Primera gráfica de estados

--- protected/views/site/reports.php
+++ protected/views/site/reports.php
@@ -12,88 +12,69 @@ $this->breadcrumbs=array(
 ?>
 
 <h1>Lista de informes disponibles</h1>
-<!-- <h3><a href='/romse/site/reportSolicitudDeCambio' >Solicitudes de cambio</a></h3>-->
 <h3><a href='../solicitudDeCambio/adminCCC' >Solicitudes de cambio</a></h3>
 
-<?php
-/*$this->widget('zii.widgets.grid.CGridView', array(
-		'dataProvider' => new CArrayDataProvider($modelChanges),
-		'columns' => array(
-				'id', 'descripcion_breve:text:Desc. Breve', 'descripcion_detallada:text:Desc. Detalle',
-				'impacto:text:Impacto', 'prioridad:text:Prioridad', 'temporizacion:text:Temporizacion',
-				'riesgos:text:Riesgos',
-				'artefacto.nombre:text:artefacto', 'creador0.username:text:Creador',
-				'probador0.username:text:Probador',
-				'desarrollador0.username:text:Desarrollador', 'nombre_estado:text:Estado',
-				array
-				(
-						'class'=>'CButtonColumn',
-						'template'=>'{view}',
-						'buttons'=>array
-						(
-								'view' => array
-								(
-										'url' =>'Yii::app()->createUrl("solicitudDeCambio/view",array("id"=>$data->id))',
-								),
-
-						),
-				)
-		),
-)); */
-?>
 <h3><a href='../proyecto/adminCCC' >Proyectos</a></h3>
 
-<!-- <h3>Proyectos</h3>-->
-
-<?php 
-/*
-$this->widget('zii.widgets.grid.CGridView', array(
-		'dataProvider' => new CArrayDataProvider($modelProjects),
-		'columns' => array(
-				'nombre', 'fecha_inicio', 
-				array
-				(
-						'class'=>'CButtonColumn',
-						'template'=>'{view}',
-						'buttons'=>array
-						(
-								'view' => array
-								(
-										'url' =>'Yii::app()->createUrl("proyecto/viewReport",array("id"=>$data->id))',
-								),
-
-						),
-				)
-		),
-));
-*/
-?>
 <h3><a href='../artefacto/adminCCC' >Artefactos</a></h3>
 
-<!-- <h3>Artefactos</h3>-->
+<h3>Gráficas</h3>
+<?php
+$changesRequestEnviado = SolicitudDeCambioController::getChangesRequestEnviado();
+$changesRequestAbierto = SolicitudDeCambioController::getChangesRequestAbierto();
+$changesRequestVerificado = SolicitudDeCambioController::getChangesRequestVerificado();
+$changesRequestDupRech = SolicitudDeCambioController::getChangesRequestDupRech();
+$changesRequestActualizada = SolicitudDeCambioController::getChangesRequestActualizada();
+$changesRequestCerrado = SolicitudDeCambioController::getChangesRequestCerrado();
 
-<?php 
-/*
-$this->widget('zii.widgets.grid.CGridView', array(
-		'dataProvider' => new CArrayDataProvider($modelArtifacts),
-		'columns' => array(
-				'nombre', 'descripcion', 'version', 
-				array
-				(
-						'class'=>'CButtonColumn',
-						'template'=>'{view}',
-						'buttons'=>array
-						(
-								'view' => array
-								(
-										'url' =>'Yii::app()->createUrl("artefacto/viewReport",array("id"=>$data->id))',
-								),
-
-						),
-				)
-		),
-));
-*/
 ?>
+<html>
+  <head>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Enviado', <?php echo $changesRequestEnviado?>],
+          ['Abierto', <?php echo $changesRequestAbierto?>],
+          ['Verificado', <?php echo $changesRequestVerificado?>],
+          ['Duplicado/Rechazado', <?php echo $changesRequestDupRech?>],
+          ['Actualizada', <?php echo $changesRequestActualizada?>],
+          ['Cerrado', <?php echo $changesRequestCerrado?>],
+        ]);
+
+        // Set chart options
+        var options = {'title':'Estados de las solicitudes que afectan al CCC',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+
+  <body>
+    <!--Div that will hold the pie chart-->
+    <div id="chart_div"></div>
+  </body>
+</html>
+
 
 
