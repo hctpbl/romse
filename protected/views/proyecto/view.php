ddb9cfde908bf683da6ace1400f1bf1836282382
Natalia <Natalia@Natalia>
2014-01-24 16:06:26 +0100

    - Control de la cantidad de atributos en las tablas - Vistas de artefactos y proyectos con elementos clickables y _view de cada uno de ellos con menos datos - Quitado ID en el perfil de los usuarios

--- protected/views/proyecto/view.php
+++ protected/views/proyecto/view.php
@@ -4,7 +4,7 @@
 
 $this->breadcrumbs=array(
 	'Proyectos'=>array('index'),
-	$model->id,
+	$model->nombre,
 );
 
 $this->menu=array(
@@ -16,12 +16,12 @@ $this->menu=array(
 );
 ?>
 
-<h1>View Proyecto #<?php echo $model->id; ?></h1>
+<h1>View Proyecto: <?php echo $model->nombre; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
-		'id',
+		//'id',
 		'nombre',
 		'fecha_inicio',
 		'fecha_fin',
