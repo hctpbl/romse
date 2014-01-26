aa43d972ea057c381787246062def0b27d1c5972
Natalia <natalia.roman.seneque@gmail.com>
2014-01-26 15:11:23 +0100

    commit para pull

--- protected/config/main.php
+++ protected/config/main.php
@@ -54,8 +54,8 @@ return array(
 		'db'=>array(
 			'connectionString' => 'mysql:host=localhost;dbname=pgpi_grupo01',
 			'emulatePrepare' => true,
-			'username' => 'user',
-			'password' => 'password',
+			'username' => 'root',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 		'errorHandler'=>array(
