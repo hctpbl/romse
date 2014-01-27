8cfffe87fe3e6b318873553ee341cfec27997a65
Natalia <Natalia@Natalia>
2014-01-27 11:41:30 +0100

    usuarios finales pueden ser probadores

--- protected/controllers/AjaxController.php
+++ protected/controllers/AjaxController.php
@@ -53,5 +53,18 @@ class AjaxController extends Controller {
 		Yii::app()->end();
 	}
 	
+	public function actionGetTesUsers() {
+		$q = 'SELECT id, CONCAT(nombre, " ", apellidos, " (", username, ")") AS value FROM usuario
+        	WHERE CONCAT(nombre, " ", apellidos, " (", username, ")") LIKE ? AND (rol_id = 3 OR rol_id = 4)';
+		$cmd = Yii::app()->db->createCommand($q);
+		$result = $cmd->query(array('%' . $_GET['term'] . '%'));
+		$data = array();
+		foreach ($result as $row) {
+			$data[] = $row;
+		}
+		echo CJSON::encode($data);
+		Yii::app()->end();
+	}
+	
 }
 ?>
